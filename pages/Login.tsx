0406cbf0eb8208de469c1604614283520687639d
Ramesh <ramesh.kumar@acutechsolutions.com>
2021-01-25 23:30:43 +0530

    My First inventory system commit

---
+++ pages/Login.tsx
@@ -0,0 +1,103 @@
+import react from "react";
+import { Formik, Form, Field } from "formik";
+import Box from "@material-ui/core/Box";
+import { TextField, Switch } from "formik-material-ui";
+import { Button, Typography, Grid, Input, InputLabel, InputAdornment } from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import LockIcon from "@material-ui/icons/Lock";
+
+interface Values {
+  username: string;
+  password: string;
+}
+
+function Login() {
+  const loginPageStyle = {
+    margin: "32px auto 37px",
+    maxWidth: "530px",
+    background: "#fff",
+    padding: "30px",
+    borderRadius: "10px",
+    boxShadow: "0px 0px 10px 10px rgba(0,0,0,0.15)",
+  };
+
+  return (
+    <div>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={6} sm={4} alignItems="center">
+          <Box sx={{ border: "1px dashed grey" }} className="loginPageStyle">
+            <h2>Login Page</h2>
+            <Formik
+              initialValues={{
+                username: "",
+                password: "",
+              }}
+              validate={(values) => {
+                const errors: Partial<Values> = {};
+                if (!values.username) {
+                  errors.username = "Please enter Username";
+                }
+                if (!values.password) {
+                  errors.password = "Please enter Password";
+                }
+                //   else if (
+                //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                //   ) {
+                //     errors.username = 'Invalid email address';
+                //   }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  setSubmitting(false);
+                  alert(JSON.stringify(values, null, 2));
+                }, 500);
+              }}
+            >
+              {({ submitForm, isSubmitting, touched, errors }) => (
+                <Form>
+                  <Field
+                    style={{ margin: "0.5rem" }}
+                    component={TextField}
+                    name="username"
+                    type="text"
+                    label="Username"
+                    //   helperText="Please Enter Username"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircleIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+
+                  <Field
+                    component={TextField}
+                    type="password"
+                    label="Password"
+                    name="password"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <LockIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+
+                  <Button variant="contained" color="primary" disabled={isSubmitting} onClick={submitForm}>
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={4}></Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Login;
